3c64c5fe208e7ea3f816104540668a6eee7c0d59
matthew-david-hawkins <hawkins.mattd@gmail.com>
2019-10-23 01:07:11 -0700

    Added Upload Data Feature

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+import base64
+import datetime
+import io
+
 import dash
+from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table
 import pandas as pd
 import plotly.graph_objs as go
 import flask
@@ -104,7 +110,7 @@ app.layout = html.Div(children=[
         multiple=True
     ),
 
-    html.Div(id='output-data-upload'),
+    
 
     html.P(children=[html.Br(),html.Br()]),
 
@@ -116,7 +122,7 @@ app.layout = html.Div(children=[
                 go.Scatter(
                     x=x_axis,
                     y=y_axis,
-                    text='myText',
+                    #text='myText',
                     mode='markers',
                     opacity=0.7,
                     marker={
@@ -162,12 +168,62 @@ app.layout = html.Div(children=[
     ),  
 
     # ------------/ Row 6 /--------------
-    html.H4(children=[html.Br(),'Bowen Turbine Data',html.Br()]),
-    generate_table(design_df)
+    html.P(children=[html.Br(),html.Br()]),
+    html.Div(id='output-data-upload')
 
    ], className='container')
 
 ])
+
+def parse_contents(contents, filename, date):
+    content_type, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+
+    return html.Div([
+        html.H5(filename),
+        html.H6(datetime.datetime.fromtimestamp(date)),
+
+        dash_table.DataTable(
+            data=df.to_dict('records'),
+            columns=[{'name': i, 'id': i} for i in df.columns]
+        ),
+
+        html.Hr(),  # horizontal line
+
+        # For debugging, display the raw contents provided by the web browser
+        html.Div('Raw Content'),
+        html.Pre(contents[0:200] + '...', style={
+            'whiteSpace': 'pre-wrap',
+            'wordBreak': 'break-all'
+        })
+    ])
+
+
+#---------/ Callbacks /------------------
+@app.callback(Output('output-data-upload', 'children'),
+              [Input('upload-data', 'contents')],
+              [State('upload-data', 'filename'),
+               State('upload-data', 'last_modified')])
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
 
 if __name__=='__main__':
     app.run_server(debug=True)
