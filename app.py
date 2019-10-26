21886a6ee1bd9d0b9cf466fe4aeb39ed7623ae51
matthew-david-hawkins <hawkins.mattd@gmail.com>
2019-10-25 19:24:06 -0700

    Configured sliders to control horizontal and vertical lines

--- app.py
+++ app.py
@@ -58,7 +58,8 @@ def piecewise_3(x, y0, y1, b0, b1, b2, x0, x1):
                          lambda x: b1*x + y1-b1*x1,
                          lambda x: b2*x + y1-b2*x1])
 
-# This function accepts x-axis data, y-axis data and the function to use (data type is function), and returns the x-data for the fit function and the y-data for the fit function
+# This function accepts x-axis data, y-axis data and the function to use (data type is function)
+# It returns the x-data and y-data for the fit function and a string of the fit equation
 def my_fx(x_data, y_data, fx):
 
     global fit_resolution
@@ -70,7 +71,31 @@ def my_fx(x_data, y_data, fx):
     # Evaluate the best fit function at {fit_resolution} number of points 
     (x_data_fit, y_data_fit) = linespace_eval(x_data, y_data, popt, eval(fx), fit_resolution)
 
-    return(x_data_fit, y_data_fit)
+    if fx == 'linear':
+
+        fit_equation = "y = " + '{:.2e}'.format(popt[0]) +'*x + '+'{:.2e}'.format(popt[1])
+
+    elif fx == 'root':
+
+        fit_equation = "y = " + '{:.2e}'.format(popt[0]) + "*sqrt(x) + " + '{:.2e}'.format(popt[1])
+
+    elif fx == 'cubic':
+        
+        fit_equation = "y = " + '{:.2e}'.format(popt[0]) + "*x^3 + " + '{:.2e}'.format(popt[1])  + "*x^2 + " + '{:.2e}'.format(popt[2])  + "*x + " + '{:.2e}'.format(popt[3])
+    
+    elif fx == 'quadratic':
+
+        fit_equation = "y = " + '{:.2e}'.format(popt[0]) + "*x^2 + " + '{:.2e}'.format(popt[1])  + "*x + " + '{:.2e}'.format(popt[2])
+
+    elif fx == 'fourth':
+
+        fit_equation = "y = " + '{:.2e}'.format(popt[0]) + "*x^4 + " + '{:.2e}'.format(popt[1])  + "*x^3 + " + '{:.2e}'.format(popt[2])  + "*x^2 + " + '{:.2e}'.format(popt[3])  + "*x + " + '{:.2e}'.format(popt[4])
+
+    elif fx == 'power':
+
+        fit_equation = "y = " + '{:.2e}'.format(popt[0]) + "*x^( " +'{:.2e}'.format(popt[1]) + ") + " + '{:.2e}'.format(popt[2])
+
+    return(x_data_fit, y_data_fit,fit_equation)
 
 # This function accepts x-axis and y-axis data, a tuple of function arguments, a function, and number evalation points.
 # This function returns a list of evenly spaced x values across the x-axis data with the corresponding y-values for the input function
@@ -161,6 +186,31 @@ app.layout = html.Div(children=[
 
     html.P(children=[html.Br(),html.Br()]),
 
+    # # ------------/ Row 6 /--------------
+    dcc.Dropdown(
+        id='fit-dropdown',
+        options=[
+            {'label': 'linear', 'value': 'linear'},
+            {'label': 'x^2', 'value': 'quadratic'},
+            {'label': 'x^3', 'value': 'cubic'},
+            {'label': 'x^4', 'value': 'fourth'},
+            {'label': 'a*x^n', 'value': 'power'},
+            {'label': 'sqrt(x)', 'value': 'root'}
+            #{'label': 'two-piece', 'value': 'piecewise_2'},
+            #{'label': 'three_piece', 'value': 'piecewise_3'},
+        ],
+        value='linear',
+        clearable=False
+    ),  
+
+    html.P(children=[html.Br(),html.Br()]),
+    html.Div([
+        html.H4(
+                children='Curve Fit Applied Only To Data Between Red Lines',
+            )
+    ], className='row justify-content-center'),
+    html.Div(id='fit-equation', className='row justify-content-center'),
+
     # ------------/ Row 4 /--------------
     html.Div([
         html.Div([
@@ -188,24 +238,6 @@ app.layout = html.Div(children=[
     # -----------/ x slider output /--------
     html.Div(id='x-slider-output-container', className='row justify-content-center'),
 
-    html.P(children=[html.Br(),html.Br()]),
-
-    # # ------------/ Row 6 /--------------
-    dcc.Dropdown(
-        id='fit-dropdown',
-        options=[
-            {'label': 'linear', 'value': 'linear'},
-            {'label': 'x^2', 'value': 'quadratic'},
-            {'label': 'x^3', 'value': 'cubic'},
-            {'label': 'x^4', 'value': 'fourth'},
-            {'label': 'a*x^n', 'value': 'power'},
-            {'label': 'sqrt(x)', 'value': 'root'}
-            #{'label': 'two-piece', 'value': 'piecewise_2'},
-            #{'label': 'three_piece', 'value': 'piecewise_3'},
-        ],
-        value='linear',
-        clearable=False
-    ),  
 
     # ------------/ Row 6 /--------------
     html.P(children=[html.Br(),html.Br()]),
@@ -260,12 +292,13 @@ def parse_contents_table(contents, filename, date, df):
         })
     ])
 
-def new_graph(df, fit_select, x_axis, y_axis):
+def new_graph(df, fit_select, x_axis, y_axis, x_value_list, y_value_list):
     
-    (x_fit, y_fit) = my_fx(x_axis, y_axis, fit_select)
+    (x_fit, y_fit, fit_equation) = my_fx(x_axis, y_axis, fit_select)
 
-    return {
+    return ({
         'data': [
+            # Inlier Data
             go.Scatter(
                 x=x_axis,
                 y=y_axis,
@@ -277,6 +310,7 @@ def new_graph(df, fit_select, x_axis, y_axis):
                     'line': {'width': 0.5, 'color': 'white'}
                 },
             ),
+            # Fit Line
             go.Scatter(
                 x=x_fit,
                 y=y_fit,
@@ -290,14 +324,69 @@ def new_graph(df, fit_select, x_axis, y_axis):
             )
         ],
         'layout': go.Layout(
+            shapes=[
+                # Vertical line representing lower x boundary
+                go.layout.Shape(
+                type="line",
+                x0=x_value_list[0],
+                y0=min(y_axis),
+                x1=x_value_list[0],
+                y1=max(y_axis),
+                opacity=0.33,
+                line=dict(
+                color="Crimson",
+                width=1,
+                dash="dashdot"),
+                ),
+                 # Vertical line representing upper x boundary
+                go.layout.Shape(
+                type="line",
+                x0=x_value_list[1],
+                y0=min(y_axis),
+                x1=x_value_list[1],
+                y1=max(y_axis),
+                opacity=0.33,
+                line=dict(
+                color="Crimson",
+                width=1,
+                dash="dashdot")
+                ),
+                # Horizontal line representing lower y boundary
+                go.layout.Shape(
+                type="line",
+                x0=min(x_axis),
+                y0=y_value_list[0],
+                x1=max(x_axis),
+                y1=y_value_list[0],
+                opacity=0.33,
+                line=dict(
+                color="Crimson",
+                width=1,
+                dash="dashdot"),
+                ),
+                 # Horizontal line representing upper y boundary
+                go.layout.Shape(
+                type="line",
+                x0=min(x_axis),
+                y0=y_value_list[1],
+                x1=max(x_axis),
+                y1=y_value_list[1],
+                opacity=0.33,
+                line=dict(
+                color="Crimson",
+                width=1,
+                dash="dashdot")
+                )
+            ],
             xaxis={'title': df.columns[0]},
             yaxis={'title': df.columns[1]},
             margin={'l': 60, 'b': 40, 't': 10, 'r': 10},
             #legend={'x': 0, 'y': 1},
             showlegend=False,
             hovermode='closest'
+
             )
-    }
+    }, fit_equation)
 
 def update_slider(data, axis):
     minimum = min(data)
@@ -329,7 +418,18 @@ def update_slider(data, axis):
 
 #-------/ Data Uploaded / -----------------
 # display the data that the user has uploaded in a table, and store data that the user uploaded into a hidden div, and update x/y sliders settings
-@app.callback([Output('output-data-upload', 'children'), Output('intermediate-value', 'children')],
+@app.callback([Output('output-data-upload', 'children'), 
+                Output('intermediate-value', 'children'),
+                Output('x-slider', 'min'), 
+                Output('x-slider', 'max'), 
+                Output('x-slider', 'marks'), 
+                Output('x-slider', 'value'), 
+                Output('x-slider', 'step'),
+                Output('y-slider', 'min'), 
+                Output('y-slider', 'max'), 
+                Output('y-slider', 'marks'), 
+                Output('y-slider', 'value'), 
+                Output('y-slider', 'step')],
               [Input('upload-data', 'contents')],
               [State('upload-data', 'filename'),
                State('upload-data', 'last_modified')])
@@ -351,23 +451,38 @@ def update_output(contents, filename, last_modified):
         upload_df = pd.read_csv('Resources/design_data.csv')
         children =[]
     
+    x_data = upload_df.iloc[:, 0]
+    y_data = upload_df.iloc[:, 1]
 
-    return (children, upload_df.to_json(date_format='iso', orient='split'))
+    (x_min, x_max, x_marks, x_value, x_step) = update_slider(x_data, 'x')
+    (y_min, y_max, y_marks, y_value, y_step) = update_slider(y_data, 'y')
+    
+
+    return (children, 
+            upload_df.to_json(date_format='iso', orient='split'),
+            x_min, 
+            x_max, 
+            x_marks, 
+            x_value, 
+            x_step,
+            y_min, 
+            y_max, 
+            y_marks, 
+            y_value, 
+            y_step, 
+            )
 
 #-------/ Fit Selected / -----------------
-@app.callback([Output('outlier-plot', 'figure'), 
-                Output('x-slider', 'min'), 
-                Output('x-slider', 'max'), 
-                Output('x-slider', 'marks'), 
-                Output('x-slider', 'value'), 
-                Output('x-slider', 'step'),
-                Output('y-slider', 'min'), 
-                Output('y-slider', 'max'), 
-                Output('y-slider', 'marks'), 
-                Output('y-slider', 'value'), 
-                Output('y-slider', 'step')],
-              [Input('fit-dropdown', 'value'), Input('intermediate-value', 'children')])
-def update_graph(selection, jsonified_data):
+@app.callback([Output('outlier-plot', 'figure'),
+                Output('fit-equation', 'children'),
+                Output('x-slider-output-container', 'children'),
+                Output('y-slider-output-container', 'children')],
+              [Input('fit-dropdown', 'value'), 
+              Input('intermediate-value', 'children'),
+              Input('x-slider', 'value'),
+              Input('y-slider', 'value')]
+              )
+def update_graph(selection, jsonified_data, x_value_list, y_value_list):
     
     if jsonified_data is not None:
         user_df = pd.read_json(jsonified_data, orient='split')
@@ -380,38 +495,33 @@ def update_graph(selection, jsonified_data):
     y_data = user_df.iloc[:, 1]
 
     # use dataframe to create a figure
-    graph = new_graph(user_df, fit_select, x_data, y_data)
+    (graph, equation) = new_graph(user_df, fit_select, x_data, y_data, x_value_list, y_value_list)
 
-    (x_min, x_max, x_marks, x_value, x_step) = update_slider(x_data, 'x')
-    (y_min, y_max, y_marks, y_value, y_step) = update_slider(y_data, 'y')
+    x_slider_reading = '{}'.format(x_value_list[0]) + ' to ' + '{}'.format(x_value_list[1])
+    y_slider_reading = html.P(children=['{}'.format(y_value_list[1]),html.Br(), ' to ', html.Br(),'{}'.format(y_value_list[0])])
 
     return (graph, 
-            x_min, 
-            x_max, 
-            x_marks, 
-            x_value, 
-            x_step,
-            y_min, 
-            y_max, 
-            y_marks, 
-            y_value, 
-            y_step)
+            equation,
+            x_slider_reading,
+            y_slider_reading
+            )
 
 #-------/ X Slider Changed / -----------------
 
-@app.callback(Output('x-slider-output-container', 'children'),
-              [Input('x-slider', 'value')])
-def update_x_slider_readout(value_list):
+# @app.callback(Output('x-slider-output-container', 'children'),
+#               [Input('x-slider', 'value')])
+# def update_x_slider_readout(value_list):
 
-    return ('{}'.format(value_list[0]) + ' to ' + '{}'.format(value_list[1]))
+#     return ('{}'.format(value_list[0]) + ' to ' + '{}'.format(value_list[1]))
 
-@app.callback(Output('y-slider-output-container', 'children'),
-              [Input('y-slider', 'value')])
-def update_y_slider_readout(value_list):
+#-------/ Y Slider Changed / -----------------
 
-    #return ('{}'.format(value_list[0]) + ' to ' + '{}'.format(value_list[1]))
-    return html.P(children=['{}'.format(value_list[1]),html.Br(), ' to ', html.Br(),'{}'.format(value_list[0])])
-    
+# @app.callback(Output('y-slider-output-container', 'children'),
+#               [Input('y-slider', 'value')])
+# def update_y_slider_readout(value_list):
+
+#     #return ('{}'.format(value_list[0]) + ' to ' + '{}'.format(value_list[1]))
+#     return html.P(children=['{}'.format(value_list[1]),html.Br(), ' to ', html.Br(),'{}'.format(value_list[0])])
 
 
 if __name__=='__main__':
