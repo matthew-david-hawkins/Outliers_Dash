98b8d387ca4d0ea5090af9ae27b7f42fd3093b6f
matthew-david-hawkins <hawkins.mattd@gmail.com>
2019-12-31 15:58:09 -0800

    modified google tracking code

--- app.py
+++ app.py
@@ -348,9 +348,7 @@ about_text3 = 'Try it!'
 
 # external JavaScript files
 external_scripts = [
-    'gtag.js',
-    'ga.js',
-    'aviator.css',
+    {'src':"https://www.googletagmanager.com/gtag/js?id=UA-136269296-4"},
     {
         'src': 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js',
         'integrity': 'sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1',
@@ -366,10 +364,7 @@ external_scripts = [
         'integrity': 'sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM',
         'crossorigin': 'anonymous'
     },
-    'https://codepen.io/chriddyp/pen/bWLwgP.css',
-    "d3.v5.min.js",
-    "download.js",
-    "static.js",
+    'https://codepen.io/chriddyp/pen/bWLwgP.css'
 ]
 
 server = flask.Flask(__name__)
